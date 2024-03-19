be907ad341cc9fdc88e3bfe7a3b27a72a7a7df8a
Arthur-Fialho <arthursouza.fp@gmail.com>
2024-03-19 11:51:57 -0300

    Initial Commit

---
+++ script/CPTH.s.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import {Script, console} from "forge-std/Script.sol";
+import {CPTH} from "../src/CPTH.sol";
+
+contract CPTHScript is Script {
+    function setUp() public {
+        CPTH cpth = new CPTH();
+        cpth(address(this), 1000);
+    }
+
+    function run() public {
+        vm.broadcast();
+    }
+}
---
+++ script/NFTHouse.s.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import {Script, console} from "forge-std/Script.sol";
+import {NFTHouse} from "../src/NFTHouse.sol";
+
+contract NFTHomeScript is Script {
+    function setUp() public {
+        NFTHouse nftHouse = new NFTHouse();
+        nftHouse();
+    }
+
+    function run() public {
+        vm.broadcast();
+    }
+}
